fafdd9a8623df4943403d9c72bc579d8df58529e
Nicholas McDowell <tekknikk@yuumii.com>
2018-03-01 02:41:34 +1100

    ensuring stripedsize has a value of 0 if nothing else

--- indexes/fee.js
+++ indexes/fee.js
@@ -103,7 +103,7 @@ FeeIndex.prototype.connect2ndOrder = function (db, txoIndex, atomic, block, call
 
     atomic.put(FEE, { height }, {
       iqr: box(feeRates),
-      size: block.strippedsize
+      size: block.strippedsize || 0
     })
     atomic.put(FEETIP, {}, block)
 
